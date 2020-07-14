3fb58bdcf465f0e5d64e4fa8cc08946cc76a22c1
Niall James <niall.james@dvla.gov.uk>
2020-07-14 15:58:41 +0100

    Rough and ready SpringBoot implementation

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -4,17 +4,23 @@ import classes.model.Vehicle;
 import classes.fileHandler.asciiIntro;
 import classes.generator.VehicleGenerator;
 import classes.illegalCombo.illegalVRMScan;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
+import java.util.Scanner;
 
+@SpringBootApplication
+@Configuration
 public class Main {
     public static HashMap<String, Vehicle> vehicle_registry =  new HashMap<>();
 
     public static void main(String[] args) {
-
         //Load title
         new asciiIntro();
-
         //Start generating vehicles
         new VehicleGenerator().generateMultipleVehicles(10000);
 
@@ -24,6 +30,23 @@ public class Main {
 
         //Print the total number of vehicles registered
         System.out.println("We have " + vehicle_registry.size() + " records");
+        Scanner scanner = new Scanner(System.in);
+        SpringApplication.run(Main.class, args);
+        String input = scanner.nextLine();
+        System.out.println("Closing");
+    }
 
+    @RestController
+    public class APIcontroller extends SpringBootServletInitializer{
+
+        public APIcontroller() {
+
+        }
+
+        @RequestMapping(value = "/hello")
+        public String findAll() {
+            String helloWorld = "hello World";
+            return helloWorld;
+        }
     }
 }
